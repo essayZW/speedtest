037fc9366041fd109698fa2002f65c74c7a359f8
essay <dazw2000@163.com>
2021-02-18 12:42:38 +0800

    解决PHP时区与数据库时区不统一问题

--- dashboard/login.php
+++ dashboard/login.php
@@ -20,11 +20,11 @@ if(isset($_GET['login'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login</title>
 </head>
 <body>
     <form action="<?= $_SERVER['PHP_SELF'] ?>?login" method="post">
-        <input type="text" name="password">
+        <input type="password" name="password">
         <input type="submit" value="login">
     </form>
 </body>
