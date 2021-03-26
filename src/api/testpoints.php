886a25991804f1fffe2ffda0b4924f6e3cd2ee78
essay <dazw2000@163.com>
2021-03-26 22:50:53 +0800

    编写测速节点增删改查API

--- src/api/testpoints.php
+++ src/api/testpoints.php
@@ -32,57 +32,33 @@ switch ($operation) {
         break;
     case 'insert':
         needAdmin();
-        $name = get($_POST, 'name');
-        $server = get($_POST, 'server');
-        // default server port is 80
-        $port = get($_POST, 'port', 80);
-        $dlURL = get($_POST, 'dlURL', '/backend/garbage.php');
-        $ulURL = get($_POST, 'ulURL', '/backend/empty.php');
-        $pingURL = get($_POST, 'pingURL', '/backend/empty.php');
-        $getIpURL = get($_POST, 'getIpURL'. '/backend/getIP.php');
-        $rep = [
-            'status' => false,
-            'code' => 200,
-            'message' => ''
-        ];
-       
-        $checkFlag = false;
-        if (!$name) {
-            $rep['message'] = 'need testpoints name';
-        }
-        else if (!$server) {
-            $rep['message'] = 'need testpoints server address';
-        }
-        else if (!is_integer($port)) {
-            $rep['message'] = 'server port must be a integer';
-        }
-        else if ($port < 0 || $port > 65535) {
-            $rep['message'] = 'server port must between 0 and 65535';
-        }
-        else if (!is_string($name) || !is_string($dlURL) || !is_string($ulURL)
-                    || !is_string($pingURL) || !is_string($getIpURL)) {
-            $rep['message'] = 'invalid params';
-        }
-        else {
-            $checkFlag = true;
-        }
-        if (!$checkFlag) {
+        $rep = validateParams();
+        if (!$rep['status']) {
+            unset($rep['data']);
             echo json_encode($rep);
             break;
         }
-
+        $name = $rep['data']['name'];
+        $server = $rep['data']['server'];
+        $port = $rep['data']['port'];
+        $dlURL = $rep['data']['dlURL'];
+        $ulURL = $rep['data']['ulURL'];
+        $pingURL = $rep['data']['pingURL'];
+        $getIpURL = $rep['data']['getIpURL'];
+        unset($rep['data']);
         try {
             $p = $conn->prepare('INSERT INTO speedtest_testpoints 
                             (`name`, `server`, `port`, `dlURL`, `ulURL`, `pingURL`, `getIpURL`)
                             VALUES (?, ?, ?, ?, ?, ?, ?)');
+            if (!$p) {
+                throw new Exception('sql prepare error');
+            }
             $p->bind_param('ssissss', $name, $server, $port, $dlURL, $ulURL, $pingURL, $getIpURL);
             $p->execute();
         }
         catch (Exception $e) {
             $rep['message'] = 'insert fail (SQL error)';
-            $checkFlag = false;
-        }
-        if (!$checkFlag) {
+            $rep['status'] = false;
             echo json_encode($rep);
             break;
         }
@@ -98,9 +74,104 @@ switch ($operation) {
         break;
     case 'update':
         needAdmin();
+        $rep = validateParams();
+        if (!$rep['status']) {
+            unset($rep['data']);
+            echo json_encode($rep);
+            break;
+        }
+        $name = $rep['data']['name'];
+        $server = $rep['data']['server'];
+        $port = $rep['data']['port'];
+        $dlURL = $rep['data']['dlURL'];
+        $ulURL = $rep['data']['ulURL'];
+        $pingURL = $rep['data']['pingURL'];
+        $getIpURL = $rep['data']['getIpURL'];
+        unset($rep['data']);
+        $id = get($_POST, 'id');
+        if ($id == null) {
+            $rep['status'] = false;
+            $rep['info']['message'] = 'need id';
+            echo json_encode($rep);
+            break;
+        }
+        $id = (int) $id;
+
+        try {
+            $p = $conn->prepare('UPDATE speedtest_testpoints SET
+                                `name` = ?, `server` = ?, `port` = ?,
+                                `dlURL` = ?, `ulURL` = ?, `pingURL` = ?, `getIpURL` = ? 
+                                WHERE id = ?');
+            if (!$p) {
+                throw new Exception('sql prepare error');
+            }
+            $p->bind_param('ssissssi', $name, $server, $port, $dlURL, $ulURL, $pingURL, $getIpURL, $id);
+            $p->execute();
+        }
+        catch (Exception $e) {
+            $rep['message'] = 'update fail (SQL error): '. $e->getMessage();
+            $rep['status'] = false;
+            echo json_encode($rep);
+            break;
+        }
+        if ($p->affected_rows > 0) {
+            $rep['status'] = true;
+            $rep['message'] = 'success';
+        }
+        else {
+            $rep['status'] = false;
+            $rep['message'] = 'update fail';
+        }
+        echo json_encode($rep);
         break;
     case 'delete':
         needAdmin();
+        $id = get($_POST, 'id');
+        if ($id == null) {
+            echo json_encode([
+                'status' => false,
+                'code' => 200,
+                'info' => 'need id'
+            ]);
+            break;
+        }
+        $id = (int) $id;
+        try {
+            $p = $conn->prepare('DELETE FROM speedtest_testpoints WHERE id = ?');
+            $p->bind_param('i', $id);
+            $p->execute();
+        }
+        catch (Exception $e) {
+            echo json_encode([
+                'status' => false,
+                'code' => 200,
+                'info' => [
+                    'message' => 'delete fail (SQL error)'
+                ]
+            ]);
+            break;
+        }
+        $deletedNums = $p->affected_rows;
+        $p->close();
+        if ($deletedNums > 0) {
+            echo json_encode([
+                'status' => true,
+                'code' => 200,
+                'info' => [
+                    'affectedRows' => $deletedNums,
+                    'message' => 'delete success'
+                ]
+            ]);
+        } else {
+            echo json_encode([
+                'status' => false,
+                'code' => 200,
+                'info' => [
+                    'affectedRows' => $deletedNums,
+                    'message' => 'delete fail'
+                ]
+            ]);
+        }
         break;
     default:
         echo json_encode([
@@ -111,3 +182,67 @@ switch ($operation) {
         break;
 }
 $conn->close();
+
+
+/**
+ * 验证参数，并对参数进行处理
+ *
+ * @return array
+ */
+function validateParams() {
+    $name = get($_POST, 'name');
+    $server = get($_POST, 'server');
+    // default server port is 80
+    $port = (int) get($_POST, 'port', 80);
+    $dlURL = get($_POST, 'dlURL', '/backend/garbage.php');
+    $ulURL = get($_POST, 'ulURL', '/backend/empty.php');
+    $pingURL = get($_POST, 'pingURL', '/backend/empty.php');
+    $getIpURL = get($_POST, 'getIpURL', '/backend/getIP.php');
+    $rep = [
+        'status' => false,
+        'message' => '',
+        'data' => []
+    ];
+
+    $checkFlag = false;
+    if (!$name) {
+        $rep['message'] = 'need testpoints name';
+    } else if (!$server) {
+        $rep['message'] = 'need testpoints server address';
+    } else if ($port < 0 || $port > 65535) {
+        $rep['message'] = 'server port must between 0 and 65535';
+    } else if (
+        !is_string($name) || !is_string($dlURL) || !is_string($ulURL)
+        || !is_string($pingURL) || !is_string($getIpURL)
+    ) {
+        $rep['message'] = 'invalid params';
+    } else {
+        $checkFlag = true;
+    }
+    if (!$checkFlag) {
+        return $rep;
+    }
+    $rep['status'] = true;
+    if (!preg_match('/^(http:\/\/|https:\/\/).+$/', $server)) {
+        // 默认http协议
+        $server = 'http://' . $server;
+    }
+    if (!isset($_POST['port'])) {
+        // 未传递默认port，根据Server address 的HTTP协议判断默认端口
+        if (strpos($server, 'https://') === 0) {
+            $port = 443;
+        } else {
+            $port = 80;
+        }
+    }
+    $rep['data'] = [
+        'name' => $name,
+        'server' => $server,
+        'port' => $port,
+        'dlURL' => $dlURL,
+        'ulURL' => $ulURL,
+        'pingURL' => $pingURL,
+        'getIpURL' => $getIpURL
+    ];
+    return $rep;
+}
